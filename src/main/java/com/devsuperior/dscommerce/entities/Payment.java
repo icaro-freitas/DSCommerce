be357d138e6c4c502325ca73657c0c60dc98cedc
icaro-freitas <icaro_freitas2014@outlook.com>
2024-07-17 21:38:32 -0300

    Equals hashcode

--- src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,7 +15,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_payment")
 public class Payment {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -57,6 +58,23 @@ public class Payment {
 
 	public void setOrder(Order order) {
 		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
