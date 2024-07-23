2be940067d4b769fd317b501d817a84e2cc5940b
icaro-freitas <icaro_freitas2014@outlook.com>
2024-07-22 22:36:50 -0300

    Fixed error handling

--- src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -37,7 +37,7 @@ public class ControllerExceptionHandler {
 	public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e,
 			HttpServletRequest request) {
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-		ValidationError err = new ValidationError(Instant.now(), status.value(), e.getMessage(),
+		ValidationError err = new ValidationError(Instant.now(), status.value(),"Dados inválidos",
 				request.getRequestURI());
 		for (FieldError f : e.getBindingResult().getFieldErrors()) {
 			err.addError(f.getField(), f.getDefaultMessage());
