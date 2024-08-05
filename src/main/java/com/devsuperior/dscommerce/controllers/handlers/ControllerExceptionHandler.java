da5c4fb1dcffe92214f428d5a1a55187cc2b0a7d
icaro-freitas <icaro_freitas2014@outlook.com>
2024-08-05 17:42:13 -0300

    Domain model User Role, seed

--- src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -34,7 +34,7 @@ public class ControllerExceptionHandler {
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e,
+	public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e,
 			HttpServletRequest request) {
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		ValidationError err = new ValidationError(Instant.now(), status.value(),"Dados inválidos",
