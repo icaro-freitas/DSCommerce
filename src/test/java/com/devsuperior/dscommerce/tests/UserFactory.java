cfd00cc11c3ed8f48033313b6d4041f7b0bbbe44
icaro-freitas <icaro_freitas2014@outlook.com>
2024-10-01 22:07:19 -0300

    Tests for User loadUserByUsername created

---
+++ src/test/java/com/devsuperior/dscommerce/tests/UserFactory.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dscommerce.tests;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+
+public class UserFactory {
+
+	public static User createClientUser() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "988888888", LocalDate.parse("2001-07-25"),
+				"$2a$10$1M.YfYbPpz03119u1OBn/.KMNS33GqRMAJ9W8MTfJ3vno4Zfsm/X6");
+		user.addRole(new Role(1L, "ROLE_CLIENT"));
+		return user;
+	}
+
+	public static User createAdminUser() {
+		User user = new User(2L, "Alex", "alex@gmail.com", "988888888", LocalDate.parse("1987-12-13"),
+				"$2a$10$1M.YfYbPpz03119u1OBn/.KMNS33GqRMAJ9W8MTfJ3vno4Zfsm/X6");
+		user.addRole(new Role(2L, "ADMIN_CLIENT"));
+		return user;
+	}
+
+	public static User createCustomClientUser(Long id, String username) {
+		User user = new User(id, "Maria", username, "988888888", LocalDate.parse("2001-07-25"),
+				"$2a$10$1M.YfYbPpz03119u1OBn/.KMNS33GqRMAJ9W8MTfJ3vno4Zfsm/X6");
+		user.addRole(new Role(1L, "ROLE_CLIENT"));
+		return user;
+	}
+
+	public static User createCustomAdminUser(Long id, String username) {
+		User user = new User(id, "alex@gmail.com", username, "988888888", LocalDate.parse("1987-12-13"),
+				"$2a$10$1M.YfYbPpz03119u1OBn/.KMNS33GqRMAJ9W8MTfJ3vno4Zfsm/X6");
+		user.addRole(new Role(2L, "ADMIN_CLIENT"));
+		return user;
+	}
+
+}
